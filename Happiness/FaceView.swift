8de80f04de07494c07ecc998f574c4c084d42181
Taku Fukushima <f.tac.mac@gmail.com>
2015-10-11 17:14:28 +0900

    Update to Xcode 7 and eliminate unnecessary indentations
    Signed-off-by: Taku Fukushima <f.tac.mac@gmail.com>

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -21,24 +21,24 @@ class FaceView: UIView {
     var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
     @IBInspectable
     var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
-    
+
     var faceCenter: CGPoint {
         return convertPoint(center, fromView: superview)
     }
-    
+
     var faceRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
-    
+
     weak var dataSource: FaceViewDataSource?
-    
+
     func scale(gesture: UIPinchGestureRecognizer) {
         if gesture.state == .Changed {
             scale *= gesture.scale
             gesture.scale = 1
         }
     }
-    
+
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
         static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
@@ -47,9 +47,9 @@ class FaceView: UIView {
         static let FaceRadiusToMouthHeightRatio: CGFloat = 3
         static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
     }
-    
+
     private enum Eye { case Left, Right }
-    
+
     private func bezierPathForEye(whichEye: Eye) -> UIBezierPath {
         let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
         let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
@@ -67,39 +67,38 @@ class FaceView: UIView {
         path.lineWidth = lineWidth
         return path
     }
-    
+
     private func bezierPathForSmile(fractionOfMaxSmile: Double) -> UIBezierPath {
         let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
         let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
         let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMouthOffsetRatio
-        
+
         let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
-        
+
         let start = CGPoint(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVerticalOffset)
         let end = CGPoint(x: start.x + mouthWidth, y: start.y)
         let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
         let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
-        
+
         let path = UIBezierPath()
         path.moveToPoint(start)
         path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
         path.lineWidth = lineWidth
         return path
     }
-    
+
     override func drawRect(rect: CGRect) {
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius,
             startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
         facePath.lineWidth = lineWidth
         color.set()
         facePath.stroke()
-        
+
         bezierPathForEye(.Left).stroke()
         bezierPathForEye(.Right).stroke()
-        
+
         let smileness = dataSource?.smilenessForFaceView(self) ?? 0.0
         let smilePath = bezierPathForSmile(smileness)
         smilePath.stroke()
     }
-
 }
